085a71a4ebd922ac4949d1841fdaa9e48679e699
sprayx <71204668+sprayx@users.noreply.github.com>
2021-06-17 11:00:43 -0300

    Initial commit

---
+++ src/main/java/xyz/spr4y/sona/Bot.java
@@ -0,0 +1,33 @@
+package xyz.spr4y.sona;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.Compression;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.security.auth.login.LoginException;
+
+public class Bot {
+
+    public static JDABuilder builder;
+
+
+    public static void main(String[] args) throws LoginException {
+        String token = Config.get("TOKEN");
+        builder = JDABuilder.createDefault(token);
+
+        builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+
+        builder.setBulkDeleteSplittingEnabled(false);
+
+        builder.setCompression(Compression.NONE);
+
+        builder.setActivity(Activity.playing("Olá Java."));
+        //builder.enableCache(GatewayIntent.GUILD_MEMBERS);
+
+        builder.build();
+
+    }
+
+}
