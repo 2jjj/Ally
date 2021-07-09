de67e6c8508b88782e3a4e2f1b49abfdc3233330
sprayx <sprayhasswag@gmail.com>
2021-07-08 23:07:38 -0300

    Q U A L I D A D E

---
+++ src/main/java/xyz/spr4y/ally/commands/QualidadeCommand.java
@@ -0,0 +1,16 @@
+package xyz.spr4y.ally.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class QualidadeCommand extends Command {
+
+    public QualidadeCommand() {
+        super.name = "qualidade";
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        //*****
+    }
+}
