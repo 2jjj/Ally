b76b84b63e05eb4e733c46e9518522e14cf6625d
sprayx <sprayhasswag@gmail.com>
2021-07-08 23:24:35 -0300

    avatar

---
+++ src/main/java/xyz/spr4y/ally/commands/AvatarCommand.java
@@ -0,0 +1,16 @@
+package xyz.spr4y.ally.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class AvatarCommand extends Command {
+
+    public AvatarCommand() {
+        super.name = "avatar";
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        //***
+    }
+}
