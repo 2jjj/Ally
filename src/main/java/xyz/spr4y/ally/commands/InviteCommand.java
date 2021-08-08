d620faa595a0bc2c80ec0030672ea0630ab68283
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-07 23:33:17 -0300

    Create InviteCommand.java

---
+++ src/main/java/xyz/spr4y/ally/commands/InviteCommand.java
@@ -0,0 +1,19 @@
+package xyz.spr4y.ally.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import java.awt.*;
+
+public class InviteCommand extends Command {
+
+    public InviteCommand() {
+        super.name = "ajuda";
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        event.reply("Olá eu sou a **"+event.getJDA().getSelfUser().getName()+"**, uma simples bot para o discord!");
+    }
+}
+
