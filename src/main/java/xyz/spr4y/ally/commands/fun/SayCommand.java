32023e3c899f7b1475315ad63783502f4327890c
Pedro <71204668+sprayx@users.noreply.github.com>
2021-08-09 20:17:55 -0300

    vamo testak

--- src/main/java/xyz/spr4y/ally/commands/fun/SayCommand.java
+++ src/main/java/xyz/spr4y/ally/commands/fun/SayCommand.java
@@ -1,4 +1,17 @@
 package xyz.spr4y.ally.commands.fun;
 
-public class SayCommand {
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import xyz.spr4y.ally.utils.VaporwaveUtils;
+
+public class SayCommand extends Command {
+
+    public SayCommand() {
+        super.name = "say";
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+      System.out.println(event.getArgs());
+    }
 }
